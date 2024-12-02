8926d07d61697ee5ee51ede975c737fe07a2545b
Julio Sesai <julio.antonio@bamaglobal.com>
2024-12-01 21:47:54 -0600

    add pagina principal

--- src/pages/PageMain.jsx
+++ src/pages/PageMain.jsx
@@ -1,30 +1,23 @@
 import React from 'react'
 import useAuth from "../hooks/useAuth"; //fn hook propio que nos permite acceder a nuestro context de autenticacion AuthContext
-
+import NavBar from '../components/NavBar';
 
 
 export default function PageMain() {
     const { auth, showAlert, navigate } = useAuth();
 
     console.log(auth._id);
-    
+
 
     return (
-        <>
-            {/* validamos por id la propiedad extraida si existe esta autenticado de lo contrario lo direccionamos al login  */}
-            {auth?._id ?
-                (
-                    <div className="bg-gray-100">
-                        <Header />
-                        <div className="md:flex md:min-h-screen">
-                            <SideBar />
-                            <main className="p-4 flex-1">
-                                <Outlet />
-                            </main>
-                        </div>
-                    </div>
-                ) : <Navigate to="/" />}
-
-        </>
+        <div className="bg-gray-100">
+            <NavBar />
+            <div className="md:flex md:min-h-screen">
+                <SideBar />
+                <main className="p-4 flex-1">
+                    
+                </main>
+            </div>
+        </div>
     )
 }
